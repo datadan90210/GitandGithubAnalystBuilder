cfff002f7d6c78fe9f5705dd241bebecdb3c9ac5
Daniel <emailLee04@gmail.com>
2025-12-03 14:15:17 -0600

    New Changes to Code

--- working_code.py
+++ working_code.py
@@ -3,7 +3,9 @@ import pandas as pd
 food = pd.read_csv(r'C:\Users\alexf\OneDrive\Documents\Tutorials\Pandas Folder\Projects\u_food_marketing.csv')
 
 food['marital_Divorced'] = food['marital_Divorced'].replace({1:5,0:0})
-food['marital_Married'] = food['marital_Married'].replace({1:4,0:0})
+
 food['marital_Single'] = food['marital_Single'].replace({1:3,0:0})
 food['marital_Together'] = food['marital_Together'].replace({1:2,0:0})
-food['marital_Widow'] = food['marital_Widow'].replace({1:1,0:0})
+
+
+food['marital_Widow'] = food['marital_Widow'].replace({1:2,0:0})
